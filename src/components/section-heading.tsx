af509a67d36b63bfb284b88b52d904a17ed77d86
aditya <adiabhiraj141@gmail.com>
2024-01-22 06:04:59 +0530

    building-in-progress-2

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 interface Section {
     heading: string;
+    // Icon: React.ReactNode;
+    Icon: React.ComponentType<{
+      className?: string; 
+    }>;
 }
-const SectionHeading = ({heading}:Section) => {
+const SectionHeading = ({heading, Icon}:Section) => {
   return (
-    <div className='w-full bg-green-300 flex justify-start'>
-        <h2 className='text-white border p-2 rounded-3xl'>
+    <div className='w-full flex justify-between items-center'>
+        <h2 className='text-white border p-1 pl-4 pr-4 rounded-3xl font-light text-sm border-gray-500 flex justify-between items-center mr-1'>
+            <Icon className='mr-1' />
             {heading}
         </h2>
+        <div className='h-px bg-gray-500 w-full'></div>
     </div>
   )
 }
--- src/sections/base.tsx
+++ src/sections/base.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import SectionHeading from '../components/section-heading';
+import {BiHome, BiDownArrowAlt} from 'react-icons/bi';
+import roundText from "../utilities/round-text.png";
 
 const Base = () => {
+
   return (
     <div className='w-full'>
-      <div>
-        <SectionHeading heading='Introduction'/>
+      <div className='relative'>
+        <SectionHeading heading='INTRODUCTION' Icon= {BiHome}/>
+      
         <h1 className='text-white text-7xl font-normal'>
           Say Hi from
           <br />
@@ -13,7 +17,13 @@ const Base = () => {
           <br />
           Webflow Developer
         </h1>
+        
+        <p className='text-gray-400 font-light text-xl '>I code beautifully simple things and i love what i do. Just simple like that!</p>
 
+        <div className='flex justify-center items-center max-w-fit border rounded-full p-2 border-gray-500 cursor-pointer fixed right-1/4'>
+          <BiDownArrowAlt className='absolute text-white font-bold text-2xl'/>
+          <img src={roundText}></img>
+        </div>
       </div>
     </div>
   )
